7032a67b0343dea2cba8a171e8c074676160c257
nagibhasan <nagibhasan132@gmail.com>
2023-02-21 17:31:05 +0600

    4th commit

---
+++ js/template.js
@@ -0,0 +1,25 @@
+const person = 'Adam Sandler';
+const person2 = 'Ben white';
+const person3 = `Donald trump`;
+
+const multiLine = 'First line text \n' + 
+'Second line of code \n' + 
+'Third line of text \n' + 
+'Fourth line of string';
+// console.log(multiLine)
+
+const newMultiLine = `First line of text
+Second line of text
+Third line of string
+Fourth line of code`;
+
+// console.log(newMultiLine)
+
+const a = 20;
+const b = 30;
+const nums = [78, 98, 45, 12, 6, 444]
+const summary = 'sum of: ' + 'snd ' + b + ' is ' + (a+b);
+// console.log(summary)
+
+const newSummary = `sum of ${a} and ${b} is: ${a+b}`
+console.log(newSummary)
